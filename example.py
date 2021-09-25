4a34eb51853b0350b9a1f8c27cc7a1fa46a797d2
Jason Luckow <ijasonluck@gmail.com>
2021-09-25 14:48:46 -0500

    pushing fix to main

--- example.py
+++ example.py
@@ -7,6 +7,10 @@ import cv2
 from gaze_tracking import GazeTracking
 
 gaze = GazeTracking()
+<<<<<<< Updated upstream
+=======
+webcam = cv2.VideoCapture(1)
+>>>>>>> Stashed changes
 
 while True:
     gaze.refresh()
@@ -27,9 +31,11 @@ while True:
 
     left_pupil = gaze.pupil_left_coords()
     right_pupil = gaze.pupil_right_coords()
-    cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+    cv2.putText(frame, "Left pupil:  " + str(left_pupil), left_pupil, cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
     cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
 
+
+    # demo = cv2.resize(frame, (1920,1080))
     cv2.imshow("Demo", frame)
 
     if cv2.waitKey(1) == 27:
